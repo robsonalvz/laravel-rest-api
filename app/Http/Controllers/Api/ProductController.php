504d84a8dc8bba362b7e01702c3c4de6ffe96494
robsonalvz <robson.alves@dce.ufpb.br>
2019-09-07 16:57:51 -0300

    Product:add store method

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+use App\Product;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProductController extends Controller
+{
+    private $product;
+
+    public function __construct(Product $product){
+        $this->product = $product;
+    }
+    public function index(){
+        $data = ['data', $this->product->all()];
+        return response()->json($data);
+    }
+    public function show(Product $id){
+        return $id;
+    }
+    public function store(Request $request){
+        $productData = $request->all();
+    }
+}
